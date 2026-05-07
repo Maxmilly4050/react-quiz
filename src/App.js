a8f2ed29e6781ad9656df2d460974c76ddce0226
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-07 23:49:37 +0300

    feat: add data fetching with useReducer and useEffect in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import Header from './Header';
 import Main from './Main';
+import { useEffect, useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        data: action.payload,
+        loading: false,
+        error: null
+      };
+    case 'dataError':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  data: null,
+  loading: true,
+  error: null
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/questions')
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(error => dispatch({ type: 'dataError', payload: error.message }));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
