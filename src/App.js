a1791800ca0a4184a0dc8e3c17e521260e4186db
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-07 23:28:38 +0300

    feat: restructure app layout and update questions data source
    - Added a new Main component to encapsulate main content.
    - Updated App component to include Main with sample content.
    - Refactored Main component to accept children as props.
    - Moved questions.json to a new data directory and restored its content.
    - Added json-server as a dependency for mock API functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './Header';
+import Main from './Main';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+
+      <Main >
+        <p>This is the main content.</p>
+      </Main>
     </div>
   );
 }
